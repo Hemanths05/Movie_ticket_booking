fe62d28506dd2e746d4738791b373fcb9a7878c5
Hemanth <hemanthsiet05@gmail.com>
2025-04-26 23:06:10 +0530

    check aws

--- server/index.js
+++ server/index.js
@@ -12,19 +12,13 @@ const adminRoutes = require('./routes/adminRoutes');
 
 dotenv.config();
 connectDB();
+
 const app = express();
-// const allowedOrigins = [
-//     'https://movie-ticket-booking-git-master-hemanth-ss-projects.vercel.app',
-//     'https://movie-ticket-booking-0igc.onrender.com',
-//     'http://cineticketmovieticketbooking.s3-website.eu-north-1.amazonaws.com'
-//   ];
-  
-  app.use(cors({
-    origin: 'http://cineticketmovieticketbooking.s3-website.eu-north-1.amazonaws.com',
+app.use(cors({
+    origin: 'https://movie-ticket-booking-git-master-hemanth-ss-projects.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }));
-  
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
